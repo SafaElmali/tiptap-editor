ced74dd03a1533667fca47c1be461aafcf7559e4
safaelmali <tsafaelmali@gmail.com>
2025-04-06 14:11:57 +0300

    add tailwind

---
+++ src/routes/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-red-500 text-3xl font-bold">Tiptap editor</h1>
+    </div>
+  );
+};
+
+export default App;
